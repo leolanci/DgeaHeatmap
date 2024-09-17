11236013d252c4c40931b669c7bcc43ddf5495ef
Lancelle <leonie.lancelle@uni-bielefeld.de>
2024-09-17 11:21:14 +0200

    R scrip and documentation first draft.

--- R/DataBuildingNanostringGeoMxDSP.R
+++ R/DataBuildingNanostringGeoMxDSP.R
@@ -2,7 +2,7 @@
 #'
 #' @param demoData A demoData object, storing all expression, annotation, and probe information linked together.
 #'
-#' @return A matrix.
+#' @return A demoData object.
 #' @export
 #'
 #' @examples
@@ -13,5 +13,74 @@ add_demoElem <- function(demoData){
     NanoStringNCTools::assayDataApply(demoData, MARGIN=2, FUN=log, base=10, elt="exprs")
   Biobase::assayDataElement(demoData, "demoElem")[1:3, 1:2]
   return(demoData)
+}
+
+#' Splitting data by group column with feature, pheno or protocol data to then get the mean.
+#'
+#' @param demoData A demoData object, storing all expression, annotation, and probe information linked together.
+#' @param vGroup Group of features in column.
+#' @param vElt Character string of the expression matrix name.
+#'
+#' @return A demoData object.
+#' @export
+#'
+#' @examples
+#' demoData <- file
+#' VGroup <-"aoi"
+#' elt <- "demoElem"
+split_data_by_column <- function(demoData, vGroup, vElt){
+  utils::head(NanoStringNCTools::esBy(demoData,
+            GROUP = vGroup,
+            FUN = function(x) {
+              NanoStringNCTools::assayDataApply(x, MARGIN = 1, FUN=mean, elt=vElt)
+            }))
+  return(demoData)
+}
+
+#' Function for automatized quality control.
+#'
+#' @param demoData A demoData object, storing all expression, annotation, and probe information linked together.
+#' @param vFlags Character string to set the flags for quality control.
+#'
+#' @return A matrix.
+#' @export
+#'
+#' @examples
+#' demoData <- file
+#' vFlags <-"QCFlags"
+
+aExprsDataQC <- function(demoData, vFlags){
+  demoData <- GeomxTools::shiftCountsOne(demoData, useDALogic = TRUE)
+  demoData <- GeomxTools::setSegmentQCFlags(demoData)
+  utils::head(Biobase::protocolData(demoData)[[vFlags]])
+  demoData <- GeomxTools::setBioProbeQCFlags(demoData)
+  Biobase::featureData(demoData)[[vFlags]][1:5, 1:4]
+  QCResultsIndex <- which(apply(Biobase::protocolData(demoData)[[vFlags]],
+                                1L , function(x) sum(x) == 0L))
+  QCPassed <- demoData[, QCResultsIndex]
+  dim(QCPassed)
+  return(QCPassed)
+}
+
+#' Function to generating a raw read count table.
+#'
+#' @param demoData A demoData object, storing all expression, annotation, and probe information linked together.
+#'
+#' @return A dataframe containing only the raw read counts that have passed quality control.
+#' @export
+#'
+#' @examples
+#' demoData <- file
+genRawReadCountTable <- function(demoData){
+  target_demoData <- GeomxTools::aggregateCounts(demoData)
+  dim(target_demoData)
+
+  GeomxTools::featureType(demoData)
+  Biobase::exprs(target_demoData)[1:5, 1:5]
+
+  matrix_Exp <- Biobase::exprs(target_demoData)
+  class(matrix_Exp)
+  df_Exp <- BiocGenerics::as.data.frame(matrix_Exp)
+  return(df_Exp)
 }
 
