f2a337c40196bdbad0630a7652b011807ac57123
Lancelle <leonie.lancelle@uni-bielefeld.de>
2025-01-13 15:16:03 +0100

    update testing to decrease package size

--- tests/testthat/test-Heatmap_building.R
+++ tests/testthat/test-Heatmap_building.R
@@ -1,4 +1,5 @@
 library(testthat)
+library(stringr)
 library(DgeaHeatmap)
 library(tinysnapshot)
 library(visualTest)
@@ -10,7 +11,7 @@ test_that("I can use the 3rd edition", {
 
 test_that("Matrix is build from an input file and one column is put as rownames", {
 
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   x <- 1
   matrix_class <- c("matrix","array")
   actual_outcome <- build_matrix(input_data, x)
@@ -22,27 +23,28 @@ test_that("Matrix is build from an input file and one column is put as rownames"
 })
 
 test_that("An factor dependent individual matrix is build from a bigger matrix", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  factors_for_matrix <- list("hippo", "pos")
+  factors_for_matrix <- list("DKD", "glomerulus")
+  print(factors_for_matrix)
   matrix_class <- c("matrix","array")
   actual_outcome <- individual_matrix(factors_for_matrix, counts_data)
-  # testing the outcome of the function to be "matrix" "array"
+    # testing the outcome of the function to be "matrix" "array"
   expect_equal(class(actual_outcome), matrix_class)
   # testing the outcome to have strings as rownames
   expect_equal(class(rownames(actual_outcome)), "character")
   # testing if number of columns that contain string in original matrix equals number of column in output matrix
   actual_output <- ncol(actual_outcome)
-  expected_output <- 9
+  expected_output <- 2
   expect_equal(actual_output, expected_output)
 
 })
 
 
 test_that("Variance of each gene is estimated and only the most variable genes are filtered from matrix", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  top_number_of_genes <- 500
+  top_number_of_genes <- 20
   highly_variable_genes <- filtering_for_top_exprGenes(counts_data, top_number_of_genes)
 
   # testing for correct number of rows
@@ -58,17 +60,18 @@ test_that("Variance of each gene is estimated and only the most variable genes a
 })
 
 test_that("Counts are scaled through Z-score scaling", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
   scaled_counts <- scale_counts(counts_data)
-  if(any(scaled_counts > 2)){
-    if(any(scaled_counts < (-2))){
+  if(any(scaled_counts < 2)){
+    if(any(scaled_counts > (-2))){
       actual_outcome <- FALSE
     }
     else {
       actual_outcome <- TRUE
     }
   }
+
   else {
     actual_outcome <- TRUE
   }
@@ -78,42 +81,15 @@ test_that("Counts are scaled through Z-score scaling", {
   expect_equal(actual_outcome, expected_outcome)
 })
 
-test_that("data ist distributed equally", {
-  #library("tinysnapshot")
-  require("tinysnapshot")
-  require("visualTest")
-  local_edition(3)
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
-  counts_data <- build_matrix(input_data, 1)
-  scaled_counts <- scale_counts(counts_data)
-  png(filename = "testShowDataDistribution.png")
-  p1 <- show_data_distribution(scaled_counts)
-  dev.off()
-  tinysnapshot::expect_snapshot_plot(p1 ,label = "test7.pdf")
-  #compare_file_text("test7.pdf", "test6.pdf")
-  #getFingerprint(file = "testShowDataDistribution.png")
-  #getFingerprint(file = "testShowDataDistributionControl.png")
-  #isSimilar(file = "testShowDataDistributionControl.png",
-   #         fingerprint = getFingerprint("testShowDataDistribution.png"),
-    #        threshold = 0.1)
-
-})
-
-#test_that("elbow plot works",{
-#  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
-#  counts_data <- build_matrix(input_data, 1)
-#  scaled_counts <- scale_counts(counts_data)
-#  p1 <- elbow_plot(2, scaled_counts)
-#  tinysnapshot::expect_snapshot_plot(p1, label = "test8.pdf")
-#})
 
 test_that("summarizing the biological replicates works", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  factors_for_matrix <- list("hippo", "pos")
+  factors_for_matrix <- list("disease")
   matrix_class <- c("matrix","array")
   indi_matrix <- individual_matrix(factors_for_matrix, counts_data)
-  probes <- list("P0_hippo_Iba1_pos", "P5_hippo_Iba1_pos", "P15_hippo_Iba1_pos","P0_hippo_Iba1_neg", "P5_hippo_Iba1_neg","P15_hippo_Iba1_neg")
+  head(indi_matrix)
+  probes <- list("DKD_glomerulus", "DKD_tubule")
   actual_outcome <- summarise_bio_replicates(indi_matrix, probes)
   # testing the outcome of the function to be "matrix" "array"
   expect_equal(class(actual_outcome), matrix_class)
@@ -121,17 +97,17 @@ test_that("summarizing the biological replicates works", {
   expect_equal(class(rownames(actual_outcome)), "character")
   # testing if number of columns that contain string in original matrix equals number of column in output matrix
   actual_output <- ncol(actual_outcome)
-  expected_output <- 6
+  expected_output <- 2
   expect_equal(actual_output, expected_output)
 })
 
 test_that("K-mean generation works",{
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  factors_for_matrix <- list("hippo", "pos")
+  factors_for_matrix <- list("disease")
   matrix_class <- c("matrix","array")
   indi_matrix <- individual_matrix(factors_for_matrix, counts_data)
-  probes <- list("P0_hippo_Iba1_pos", "P5_hippo_Iba1_pos", "P15_hippo_Iba1_pos")
+  probes <- list("DKD_glomerulus", "DKD_tubule")
   SumTable <- summarise_bio_replicates(indi_matrix, probes)
   K_meanTable <- Kmean_generation(SumTable, 1, 3)
   colNumSum <- ncol(SumTable)
@@ -140,21 +116,21 @@ test_that("K-mean generation works",{
   # testing that a new column is added
   expect_equal(actual_outcome, expected_outcome)
   # testing the outcome of the function to be "matrix" "array"
-  actual_outcome <- Kmean_generation(SumTable, 1, 3)
+  actual_outcome <- Kmean_generation(SumTable, 1, 1)
   expect_equal(class(actual_outcome), matrix_class)
 
 })
 
 test_that("function makes list of most variable genes (rows) of each cluster", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  factors_for_matrix <- list("hippo", "pos")
+  factors_for_matrix <- list("disease")
   matrix_class <- c("matrix","array")
   indi_matrix <- individual_matrix(factors_for_matrix, counts_data)
-  probes <- list("P0_hippo_Iba1_pos", "P5_hippo_Iba1_pos", "P15_hippo_Iba1_pos")
+  probes <- list("DKD_glomerulus", "DKD_tubule")
   SumTable <- summarise_bio_replicates(indi_matrix, probes)
-  K_meanTable <- Kmean_generation(SumTable, 1, 3)
-  actualOutput <- most_variable_genes(K_meanTable, 5, 3)
+  K_meanTable <- Kmean_generation(SumTable, 1, 1)
+  actualOutput <- most_variable_genes(K_meanTable, 1, 2)
   # testing the outcome is a list
   expected_outcome <- "list"
   expect_equal(class(actualOutput), expected_outcome)
@@ -162,15 +138,15 @@ test_that("function makes list of most variable genes (rows) of each cluster", {
 })
 
 test_that("performing k_mean clustering outside of the heatmap works", {
-  input_data <- read.csv(test_path("1_Counts_All_Regions_All_Ages_Pos_Neg.csv"))
+  input_data <- read.csv(test_path("testfile_counts.csv"))
   counts_data <- build_matrix(input_data, 1)
-  factors_for_matrix <- list("hippo", "pos")
+  factors_for_matrix <- list("disease")
   matrix_class <- c("matrix","array")
   indi_matrix <- individual_matrix(factors_for_matrix, counts_data)
-  probes <- list("P0_hippo_Iba1_pos", "P5_hippo_Iba1_pos", "P15_hippo_Iba1_pos")
+  probes <- list("DKD_glomerulus", "DKD_tubule")
   SumTable <- summarise_bio_replicates(indi_matrix, probes)
-  K_meanTable <- Kmean_generation(SumTable, 1, 3)
-  actualOutput <- performing_kMeans(SumTable, 3)
+  K_meanTable <- Kmean_generation(SumTable, 1, 1)
+  actualOutput <- performing_kMeans(SumTable, 1)
   # testing that outcomes is right class
   expect_outcome <- "character"
   expect_equal(class(actualOutput), expect_outcome)
