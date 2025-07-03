f5fbe9ebb5de567801ef7d55c12d6b9a539f6a30
Lancelle <leonie.lancelle@uni-bielefeld.de>
2025-07-03 15:50:47 +0200

    added function DGEA_DESeq2

--- R/DEA_functions.R
+++ R/DEA_functions.R
@@ -78,3 +78,70 @@ DGEALimma <- function(
   ))
 
 }
+
+#' Performs a differential expression analysis based on DESeq2
+#'
+#' @param rawCounts A matrix containing raw counts (integers).
+#' @param metadata A dataframe containing the meta data information of the raw counts matrix.
+#' @param grouping_columns A list of the columns names in the metadata file that are used to generate the groups for the DEA that are compared with each other.
+#' @param comparisons An element of the class list, that contains optional information on specific comparisons that are to be conducted (simplifies the results).
+#'
+#' @return A list containing the voom data, fitting of the linear model, results of the contrasts, and the generated design.
+#' @export
+#'
+#' @examples
+#' rawCounts <- matrix
+#' metadata <- data.frame
+#' grouping_columns <- c("segment", "region", "class", "slide_name")
+#' comparisons <- list("Comp1 = c("Group_a", "Group_b"), "Comp2" = c("Group_c", "Group_d"))
+#'
+DGEA_DESeq2 <- function (rawCounts, metadata, grouping_columns, comparisons){
+  # Filter genes with non-zero counts in at least n samples (e.g., n = 2)
+  keep <- rowSums(rawCounts > 0) >= 2
+  filtered_counts <- rawCounts[keep, ]
+
+  # Combine grouping columns into a composite factor
+  comp <- apply(metadata[, grouping_columns, drop = FALSE], 1, paste, collapse = "_")
+  comp <- gsub(" ", "_", comp)
+  metadata$comp <- make.names(comp)
+  metadata$comp <- factor(metadata$comp)
+
+  # Build DESeq2 dataset
+  dds <- DESeq2::DESeqDataSetFromMatrix(
+    countData = filtered_counts,
+    colData = metadata,
+    design = ~ 0 + comp
+  )
+
+  # Run DESeq2 pipeline
+  dds <- DESeq2::DESeq(dds, sfType = "poscounts")
+
+  results_list <- list()
+
+  for (name in names(comparisons)) {
+    contrast_pair <- trimws(comparisons[[name]])
+
+    # Check levels exist in your comp factor:
+    if (all(contrast_pair %in% levels(SummarizedExperiment::colData(dds)$comp))) {
+      res <- tryCatch({
+        DESeq2::results(dds, contrast = c("comp", contrast_pair[1], contrast_pair[2]))
+      }, error = function(e) {
+        warning(paste("Contrast failed:", name, "->", e$message))
+        NULL
+      })
+
+      if (!is.null(res)) {
+        results_list[[name]] <- as.data.frame(res)
+      }
+    } else {
+      warning(paste("Contrast failed:", name, "-> One or both levels not found in comp factor levels"))
+      print(setdiff(contrast_pair, levels(colData(dds)$comp)))
+    }
+  }
+  contrast_names <- names(comparisons)
+  return(list(
+    dds = dds,
+    results = results_list,
+    contrasts = contrast_names
+  ))
+}
